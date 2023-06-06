90efcbde1c91377d75dce12c3a583b85467838ec
clever-cr <cleverumuhuza@gmail.com>
2023-06-06 17:29:50 +0200

    refactor

--- exercise8.js
+++ exercise8.js
@@ -1,4 +1,3 @@
 function discount(totalamount) {
-    const disc = totalamount > 100 ? totalamount * 0.1 : totalamount * 0.05;
-    return disc;
+    return totalamount > 100 ? totalamount * 0.1 : totalamount * 0.05;
 }
